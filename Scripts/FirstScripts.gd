11d77c6d1828cd0936c83af13ed059409bf2d6cb
delingfengyu0711 <2978303864@qq.com>
2025-10-21 17:12:30 +0800

    Update project

--- Scripts/FirstScripts.gd
+++ Scripts/FirstScripts.gd
@@ -1,11 +1,3 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+version https://git-lfs.github.com/spec/v1
+oid sha256:63cbf485c8ff1d37fe1b2437b6de65b418314a6b7254c8145bfe34dd7b0c7359
+size 260
---
+++ Scripts/FollowMouse.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:731855c941bc313aed0e2faac5b00c9e130f273f66d4488eaa4b073dde819f95
+size 208
